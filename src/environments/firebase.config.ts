12a28752b282b55aad1e68ce63df7ff44aa0f1be
Juan José Rendón <juan.rendon@yuxiglobal.com>
2018-07-08 16:35:19 -0500

    Initial project set up

---
+++ src/environments/firebase.config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+    apiKey: 'AIzaSyDKu3lYF_3p7No1OGeDyKMiROUfdujF7uE',
+    authDomain: 'pwa-push-notifications-108e3.firebaseapp.com',
+    databaseURL: 'https://pwa-push-notifications-108e3.firebaseio.com',
+    projectId: 'pwa-push-notifications-108e3',
+    storageBucket: 'pwa-push-notifications-108e3.appspot.com',
+    messagingSenderId: '446519166896',
+};
