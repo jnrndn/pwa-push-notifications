a6a6218e4e038dce077662cd69ac4bdb50b8f2d5
Juan José Rendón <juan.rendon@yuxiglobal.com>
2018-07-15 16:01:29 -0500

    Feat: form to send push notification

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,11 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-import { AppComponent } from './app.component';
+import { MatButtonModule, MatCardModule, MatInputModule } from '@angular/material';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ServiceWorkerModule } from '@angular/service-worker';
+
 import { environment } from '../environments/environment';
+import { AppComponent } from './app.component';
 import { FormComponent } from './form/form.component';
 
 @NgModule({
@@ -13,7 +15,11 @@ import { FormComponent } from './form/form.component';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     ServiceWorkerModule.register('./ngsw-worker.js', { enabled: environment.production }),
+    MatButtonModule,
+    MatCardModule,
+    MatInputModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
